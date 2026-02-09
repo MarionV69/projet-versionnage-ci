6be43876a92b96700d6470def940260c27fa3245
Yannick Olinga <ton-email@exemple.com>
2026-02-09 16:20:45 +0100

    fix: space between heroSection and Mapsection

---
+++ src/components/Cards.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Cards() {
+    return (
+        <div>
+            <div className="big_div">
+                
+                <div className="text">
+                    <h1>Suivez les points rouges pour nous retrouver</h1>
+                    <h3>   Nous disposons de +20000 points de charge dans toutes la France</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
