c9255850afa76bc2dd46abd7abd9f1177c8612aa
Yannick Olinga <yannickolinga@Yannicks-MacBook-Air.local>
2026-02-09 14:45:21 +0100

    feat: initialize map-section(#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
+import Map from "./components/map";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Map.tsx
@@ -0,0 +1,14 @@
+
+import "../../src/index.css";
+
+const MapComponent = () => {
+ 
+
+  return (
+   <div className="map-container">
+    
+   </div>
+  );
+};
+
+export default MapComponent;
